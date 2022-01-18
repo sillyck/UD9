ab451aae4b34dd06f8796f9a82e42791246ea054
TheBerayita <97508463+TheBerayita@users.noreply.github.com>
2022-01-18 12:42:43 +0100

    Commit

--- src/aula.java
+++ src/aula.java
@@ -3,11 +3,27 @@ public class aula {
 	protected int id;
 	protected int aforo;
 	protected Materia asignatura;
+	protected static estudiantes alumnos[];
+	protected static int contador=0;
+	
 	public aula(int id, int aforo, Materia asignatura) {
 		super();
 		this.id = id;
 		this.aforo = aforo;
 		this.asignatura = asignatura;
+		this.alumnos=new estudiantes[aforo];
+	}
+	public static void añadir(estudiantes a) {
+		alumnos[contador]=a;
+		contador++;
+	}
+	@Override
+	public String toString() {
+		for (int i = 0; i < alumnos.length; i++) {
+			System.out.println(alumnos[i]);
+		}
+		return "aula [id=" + id + ", aforo=" + aforo + ", asignatura=" + asignatura + "]";
+		
 	}
 	
 }
