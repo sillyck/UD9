fd0d075305c03f74876e1bec3404911c365148c1
TheBerayita <97508463+TheBerayita@users.noreply.github.com>
2022-01-19 12:32:49 +0100

    Commit

--- src/aulaApp.java
+++ src/aulaApp.java
@@ -1,3 +1,8 @@
+import clases.Materia;
+import clases.aula;
+import clases.estudiantes;
+import clases.profesor;
+
 /*
  * Autores: 
  * Pau Robuste
--- src/clases/persona.java
+++ src/clases/persona.java
@@ -1,3 +1,4 @@
+package clases;
 /*
  * Autores: 
  * Pau Robuste
